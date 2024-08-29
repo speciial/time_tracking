a671194f62b86acc3f9fad1f13b319e8a13e7e9d
Christian <c.christian.heidemann@gmail.com>
2024-08-29 18:11:12 +0200

    Provided own implementation of min and max macros

--- source/common_fnc.h
+++ source/common_fnc.h
@@ -1,6 +1,17 @@
 #ifndef COMMON_FNC_H
 #define COMMON_FNC_H
 
+#ifdef min
+#undef min
+#endif
+
+#ifdef max
+#undef max
+#endif
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
 int stringToInt(const char *string);
 
 #endif // COMMON_FNC_H
