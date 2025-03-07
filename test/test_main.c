d05e0415d6ad7728c611f31864fa7aa54299f5e5
Christian <c.christian.heidemann@gmail.com>
2025-03-07 23:13:59 +0100

    Fixed working directory for tests

--- test/test_main.c
+++ test/test_main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <time_tracking.h>
 
 int main ()
 {
     printf("Testing\n");
+
+    FILE *test = fopen("test_record.ttr", "r");
+
+    if (!test)
+    {
+        return 2;
+    }
 
     if (!DummyFunction())
     {
